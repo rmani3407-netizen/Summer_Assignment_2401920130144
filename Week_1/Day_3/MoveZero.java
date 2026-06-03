565a46f5058a62824a9a165246ecc7b760ccf107
rmani3407-netizen <rmani3407@gmail.com>
2026-06-03 11:48:18 +0530

    Added MoveZero solution in day_3

---
+++ Week_1/Day_3/MoveZero.java
@@ -0,0 +1,18 @@
+public class MoveZero {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+
+        // Move all non-zero elements to the front
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+        }
+
+        // Fill remaining positions with zeros
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
+    }
+}
+
