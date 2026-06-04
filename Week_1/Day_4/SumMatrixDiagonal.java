2c57cf6cb278a2b146baf3c290e55c6d42042229
rmani3407-netizen <rmani3407@gmail.com>
2026-06-04 19:19:58 +0530

    Add Day 4 solutions

---
+++ Week_1/Day_4/SumMatrixDiagonal.java
@@ -0,0 +1,16 @@
+package Week_1.Day_4;
+
+public class SumMatrixDiagonal {
+    public int diagonalSum(int [][]mat){
+        int n=mat.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=mat[i][i];
+            sum+=mat[i][n-1-i];
+        }
+        if(n%2==1){
+            sum-=mat[n/2][n/2];
+        }
+        return sum;
+    }
+}
