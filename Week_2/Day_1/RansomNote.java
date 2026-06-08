c8957c5fe5ad86c095a6bc06db498c924450545b
rmani3407-netizen <rmani3407@gmail.com>
2026-06-08 15:43:03 +0530

    Complete merge

---
+++ Week_2/Day_1/RansomNote.java
@@ -0,0 +1,21 @@
+package Week_2.Day_1;
+
+public class RansomNote {
+  
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+
+        for (char c : magazine.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (--count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
