04eb75c93e29cae55e42a358028eda1a44deade1
rmani3407-netizen <rmani3407@gmail.com>
2026-06-10 22:53:13 +0530

    Add Day_3 isSubsequance solution

---
+++ Week_2/Day_3/isSubsequance.java
@@ -0,0 +1,15 @@
+package Week_2.Day_3;
+
+public class isSubsequance {
+        public boolean isSubsequence(String s, String t) {
+        int i=0;
+        int j=0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+        return i==s.length();
+    }
+}
