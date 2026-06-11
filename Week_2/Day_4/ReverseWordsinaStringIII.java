2cba527194ee4b8d78a848e6caa6c7c18c568df9
rmani3407-netizen <rmani3407@gmail.com>
2026-06-11 11:34:37 +0530

    Add Day_4 ReverseWordsinaStringIII  solution

---
+++ Week_2/Day_4/ReverseWordsinaStringIII.java
@@ -0,0 +1,25 @@
+package Week_2.Day_4;
+
+public class ReverseWordsinaStringIII {
+    public String reverseWordString(String s){
+        char [] arr= s.toCharArray();
+        int start=0;
+        for(int end=0;end<=arr.length;end++){
+            if(end==s.length() || arr[end]==' '){
+                reverse(arr,start,end-1);
+                start=end+1;
+            }
+        }
+        return new String(arr);
+    }
+    private void reverse(char [] arr, int left ,int right){
+        while (left<right) {
+            char temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+    }
+
+}
