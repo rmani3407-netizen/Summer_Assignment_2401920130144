6e698eb6e300167d2dc644b2ab62fa4d25a1c32a
rmani3407-netizen <rmani3407@gmail.com>
2026-06-22 16:26:23 +0530

    Add Week_4 Day_1 Solution

---
+++ Week_4/Day_1/MaximumDepthofBinaryTree.java
@@ -0,0 +1,23 @@
+package Week_4.Day_1;
+import java.util.*;
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val){
+        this.val=val;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+
+public class MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        return 1+ Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+    
+}
